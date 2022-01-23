15d95e6f6908dc57e9daa8019a454bda128a19f0
Tom-website <tomtom22006@gmail.com>
2022-01-23 11:55:40 +0200

    Edit in some files and put first file JavaScript and Edit newBooks, topBooks and some thinks

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// New Books
+let newBooks = `
+  <div class="title-section">
+    <p>احدث الكتب</p>
+  </div>
+  <div class="books">
+    <div class="book">
+      <a href="Book/book-1.html"
+        ><img src="image/images-books/Book-1.jpg" alt=""
+      /></a>
+    </div>
+    <div class="book">
+      <a href="Book/book-2.html"
+        ><img src="image/images-books/Book-2.jpg" alt=""
+      /></a>
+    </div>
+    <div class="book">
+      <a href="Book/book-3.html"
+        ><img src="image/images-books/book-3.jpg" alt=""
+      /></a>
+    </div>
+    <div class="book">
+      <a href="Book/book-4.html"
+        ><img src="image/images-books/book-4.jpg" alt=""
+      /></a>
+    </div>
+  </div>
+  <div class="links">
+    <a href="new-books.html" class="link">&lt;&lt; المزيد</a>
+  </div>
+`;
+document.getElementById("newBooksSection").innerHTML = newBooks;
+/* Put This Code in Day 2022/01/21 */
+
+// Top Books
+let topBooks = `
+  <div class="title-section">
+    <p>افضل الكتب</p>
+  </div>
+  <div class="books">
+  <div class="book">
+    <a href="Book/book-1.html"
+      ><img src="image/images-books/Book-1.jpg" alt=""
+    /></a>
+  </div>
+  <div class="book">
+    <a href="Book/book-2.html"
+      ><img src="https://lh4.googleusercontent.com/73NHHAG9xPJCB8oYQ9LW2nqVlYLIV9tyJjLMjKVM1f_exDbOutRMub2m8cMmAw8iAc7iONLb-FKKAYzTzUjP=w1024-h657" alt=""
+    /></a>
+  </div>
+  <div class="book">
+    <a href="http://"
+      ><img src="image/images-books/book-6.png" alt=""
+    /></a>
+  </div>
+  <div class="book">
+    <a href="http://"
+      ><img src="image/images-books/book-8.png" alt=""
+    /></a>
+  </div>
+  </div>
+  <div class="links">
+  <a href="top-books/top-books.html" class="link">&lt;&lt; المزيد</a>
+  </div>
+`;
+document.getElementById("topBooksSection").innerHTML = topBooks;
+/* Put This Code in Day 2022/01/21 */
